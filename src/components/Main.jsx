0f2dd5dac209b91df5fef7414a44b1ce88639e55
giuseppezaccato <giuseppezaccato@gmail.com>
2025-02-18 20:03:16 +0100

    ADD: main+style

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,3 +1,9 @@
+import comics from "../assets/img/buy-comics-digital-comics.png"
+import merch from "../assets/img/buy-comics-merchandise.png"
+import shop from "../assets/img/buy-comics-shop-locator.png"
+import sub from "../assets/img/buy-comics-subscriptions.png"
+import buy from "../assets/img/buy-dc-power-visa.svg"
+
 const Main = () => {
 
     return (
@@ -7,24 +13,17 @@ const Main = () => {
 
             </main>
             <nav>
-                <ul>
-                    <li><a href="#"><img src="./assets/buy-comics-digital-comics.png" alt="" /> buy-comics-digital-comics.png  </a></li>
-                    <li><a href="#">  </a></li>
-                    <li><a href="#">  </a></li>
-                    <li><a href="#">  </a></li>
-                    <li><a href="#">  </a></li>
+                <ul className="container">
+                    <li><a href="#"> <img className="resize" src={comics} alt="comics" /></a><div>buy comics</div></li>
+                    <li><a href="#"> <img className="resize" src={merch} alt="merch" /></a><div>buy comics</div></li>
+                    <li><a href="#"> <img className="resize" src={shop} alt="shop" /></a><div>buy comics</div></li>
+                    <li><a href="#"> <img className="resize" src={sub} alt="sub" /></a><div>buy comics</div></li>
+                    <li><a href="#"> <img className="resize" src={buy} alt="buy" /></a><div>buy comics</div></li>
                 </ul>
 
-
             </nav>
-
-
         </>
-
-
     )
 }
-
-
 
 export default Main
