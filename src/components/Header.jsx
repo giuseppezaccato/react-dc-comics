e626d2626ad0ca814d8b939c5248b9b6a86b87af
giuseppezaccato <giuseppezaccato@gmail.com>
2025-02-18 18:11:48 +0100

    ADD: basic links(imp/exp) + Header

---
+++ src/components/Header.jsx
@@ -0,0 +1,31 @@
+const Header = () => {
+    return (
+        <header className="container ">
+
+            <figure>
+                <img src="./dc-logo.png" alt="Logo" />
+            </figure>
+
+            <nav>
+                <ul className="uppercase flex">
+                    <li><a href="#">Characters</a></li>
+                    <li><a href="#" >comics</a></li>
+                    <li><a href="#">Movies</a></li>
+                    <li><a href="#">Tv</a></li>
+                    <li><a href="#" className="active">games</a></li>
+                    <li><a href="#">collectibles</a></li>
+                    <li><a href="#">videos</a></li>
+                    <li><a href="#">fans</a></li>
+                    <li><a href="#">news</a></li>
+                    <li><a href="#">shop</a></li>
+                </ul>
+            </nav>
+
+
+
+
+        </header>
+    )
+}
+
+export default Header
---
+++ src/components/Main.jsx
@@ -0,0 +1,30 @@
+const Main = () => {
+
+    return (
+        <>
+            <main >
+                <h1>contenuto fumetti qui</h1>
+
+            </main>
+            <nav>
+                <ul>
+                    <li><a href="#"><img src="./assets/buy-comics-digital-comics.png" alt="" /> buy-comics-digital-comics.png  </a></li>
+                    <li><a href="#">  </a></li>
+                    <li><a href="#">  </a></li>
+                    <li><a href="#">  </a></li>
+                    <li><a href="#">  </a></li>
+                </ul>
+
+
+            </nav>
+
+
+        </>
+
+
+    )
+}
+
+
+
+export default Main
