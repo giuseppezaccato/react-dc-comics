707fd4658de96ce56de35606f5b9ff52351851be
giuseppezaccato <giuseppezaccato@gmail.com>
2025-02-18 23:03:17 +0100

    ADD: :root &  :hover

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,16 +3,18 @@ const Header = () => {
         <header className="container ">
 
             <figure>
-                <img src="./dc-logo.png" alt="Logo" />
+                <a href="">
+                    <img src="./dc-logo.png" alt="Logo" />
+                </a>
             </figure>
 
             <nav>
                 <ul>
                     <li><a href="#">Characters</a></li>
-                    <li><a href="#" >comics</a></li>
+                    <li><a href="#" className="active">comics</a></li>
                     <li><a href="#">Movies</a></li>
                     <li><a href="#">Tv</a></li>
-                    <li><a href="#" className="active">games</a></li>
+                    <li><a href="#" >games</a></li>
                     <li><a href="#">collectibles</a></li>
                     <li><a href="#">videos</a></li>
                     <li><a href="#">fans</a></li>
