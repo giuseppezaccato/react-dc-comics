7e04454504561d935bac50b9d00ff860e263593d
giuseppezaccato <giuseppezaccato@gmail.com>
2025-02-19 18:33:45 +0100

    FIX: data/folder + App link

--- src/App.jsx
+++ src/App.jsx
@@ -3,21 +3,23 @@ import Header from './components/Header'
 import Main from './components/Main'
 import Footer from './components/Footer'
 
+//task import dai data links.js
+import links from "./data/links"
 
 function App() {
 
-  const links = [
-    { id: 1, text: 'Characters', url: '#', current: false },
-    { id: 2, text: 'Comics', url: '#', current: true },
-    { id: 3, text: 'Movies', url: '#', current: false },
-    { id: 4, text: 'TV', url: '#', current: false },
-    { id: 5, text: 'Games', url: '#', current: false },
-    { id: 6, text: 'Collectibles', url: '#', current: false },
-    { id: 7, text: 'Videos', url: '#', current: false },
-    { id: 8, text: 'Fans', url: '#', current: false },
-    { id: 9, text: 'News', url: '#', current: false },
-    { id: 10, text: 'Shop', url: '#', current: false },
-  ];
+  // const links = [
+  //   { id: 1, text: 'Characters', url: '#', current: false },
+  //   { id: 2, text: 'Comics', url: '#', current: true },
+  //   { id: 3, text: 'Movies', url: '#', current: false },
+  //   { id: 4, text: 'TV', url: '#', current: false },
+  //   { id: 5, text: 'Games', url: '#', current: false },
+  //   { id: 6, text: 'Collectibles', url: '#', current: false },
+  //   { id: 7, text: 'Videos', url: '#', current: false },
+  //   { id: 8, text: 'Fans', url: '#', current: false },
+  //   { id: 9, text: 'News', url: '#', current: false },
+  //   { id: 10, text: 'Shop', url: '#', current: false },
+  // ];
 
 
   return (
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -19,19 +19,19 @@ const Header = (headerProps) => {
 
 //task sostituisco dinamicamente con <NavBar /> il contenuto statico sottostante
 {/*             
-            <nav>
-                <ul>
-                    <li><a href="#">Characters</a></li>
-                    <li><a href="#" className="active">comics</a></li>
-                    <li><a href="#">Movies</a></li>
-                    <li><a href="#">Tv</a></li>
-                    <li><a href="#" >games</a></li>
-                    <li><a href="#">collectibles</a></li>
-                    <li><a href="#">videos</a></li>
-                    <li><a href="#">fans</a></li>
-                    <li><a href="#">news</a></li>
-                    <li><a href="#">shop</a></li>
-                </ul>
-            </nav> */}
+    <nav>
+        <ul>
+            <li><a href="#">Characters</a></li>
+            <li><a href="#" className="active">comics</a></li>
+            <li><a href="#">Movies</a></li>
+            <li><a href="#">Tv</a></li>
+            <li><a href="#" >games</a></li>
+            <li><a href="#">collectibles</a></li>
+            <li><a href="#">videos</a></li>
+            <li><a href="#">fans</a></li>
+            <li><a href="#">news</a></li>
+            <li><a href="#">shop</a></li>
+        </ul>
+    </nav> */}
 
 export default Header
